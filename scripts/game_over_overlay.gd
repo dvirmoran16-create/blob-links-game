5af69f3d6dbc559a33cc8e1c5a4a73eb0ec25348
Dvirm <Dvirm@campus.technion.ac.il>
2026-04-09 00:35:47 +0300

    changed wording a bit in menus and added a quit game button in game over

--- scripts/game_over_overlay.gd
+++ scripts/game_over_overlay.gd
@@ -2,11 +2,13 @@ extends CanvasLayer
 
 @onready var restart_button = $Control/VBoxContainer/RestartButton
 @onready var main_menu_button = $Control/VBoxContainer/MainMenuButton
+@onready var exit_button = $Control/VBoxContainer/ExitButton
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	restart_button.pressed.connect(_on_restart_pressed)
 	main_menu_button.pressed.connect(_on_main_menu_pressed)
+	exit_button.pressed.connect(_on_exit_pressed)
 	hide()
 
 func _on_restart_pressed():
@@ -16,4 +18,6 @@ func _on_restart_pressed():
 func _on_main_menu_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_scenes/main_menu.tscn")
-	
+
+func _on_exit_pressed():
+		get_tree().quit()
