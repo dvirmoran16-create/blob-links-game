3f70807fe1074602f587b1cef8e68889174bdccd
Dvirm <Dvirm@campus.technion.ac.il>
2026-04-30 22:16:50 +0300

    cleaned up a bit the blob line drawer

--- scenes/blob_scenes/blob_lines_drawer.gd
+++ scenes/blob_scenes/blob_lines_drawer.gd
@@ -15,27 +15,20 @@ func _process(_delta):
 	queue_redraw()
 
 func _draw():
-	if not player:
-		return
-	
-	var blobs = get_tree().get_nodes_in_group("blobs")
-	var is_dashed = false
-	
-	if is_instance_valid(blob):
-		var is_frame_blink = get_is_frame_blink(blob)
-		var line_color = normal_line_color
-		var line_width = normal_line_width
-		if blob.blob_status == Blob.BlobStatus.OUT_OF_RANGE:
-			line_color = inactive_line_color
-		elif blob.blob_status == Blob.BlobStatus.HIGHLIGHTED:
-			line_color = highlight_line_color
-			line_width = highlight_line_width
-		if is_frame_blink:
-			line_color.a *= 0.3
-		if blob.global_position.distance_squared_to(player.global_position) >= dashed_line_range_squared:
-			draw_dashed_line(Vector2.ZERO, to_local(player.global_position), line_color, line_width, 10)
-		else:
-			draw_line(Vector2.ZERO, to_local(player.global_position), line_color, line_width)
+	var is_frame_blink = get_is_frame_blink(blob)
+	var line_color = normal_line_color
+	var line_width = normal_line_width
+	if blob.blob_status == Blob.BlobStatus.OUT_OF_RANGE:
+		line_color = inactive_line_color
+	elif blob.blob_status == Blob.BlobStatus.HIGHLIGHTED:
+		line_color = highlight_line_color
+		line_width = highlight_line_width
+	if is_frame_blink:
+		line_color.a *= 0.3
+	if blob.global_position.distance_squared_to(player.global_position) >= dashed_line_range_squared:
+		draw_dashed_line(Vector2.ZERO, to_local(player.global_position), line_color, line_width, 10)
+	else:
+		draw_line(Vector2.ZERO, to_local(player.global_position), line_color, line_width)
 
 func get_is_frame_blink(blob: Blob):
 	var is_frame_blink = false
